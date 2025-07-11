9586101af4dc608af42074e0c6914c1ee5f1bd43
Clément <clemmoller@gmail.com>
2025-07-11 15:46:11 +0200

    :sparkles: Fill setup section.

--- test/Setup.sol
+++ test/Setup.sol
@@ -4,8 +4,21 @@ pragma solidity 0.8.29;
 // Test imports
 import { Base } from "test/Base.sol";
 
+// Origin Dollar
+import { InitializableAbstractStrategy } from "@origin-dollar/utils/InitializableAbstractStrategy.sol";
+import { CompoundingStakingSSVStrategy } from
+    "@origin-dollar/strategies/NativeStaking/CompoundingStakingSSVStrategy.sol";
+import { CompoundingStakingSSVStrategyProxy } from "@origin-dollar/proxies/Proxies.sol";
+
 // Mocks
 import { MockERC20 } from "@solmate/test/utils/mocks/MockERC20.sol";
+import { MockSSVNetwork } from "test/mocks/MockSSVNetwork.sol";
+import { MockBeaconRoots } from "test/mocks/MockBeaconRoots.sol";
+import { MockBeaconOracle } from "test/mocks/MockBeaconOracle.sol";
+import { MockBeaconProofs } from "test/mocks/MockBeaconProofs.sol";
+import { MockDepositContract } from "test/mocks/MockDepositContract.sol";
+import { MockWithdrawalRequest } from "test/mocks/MockWithdrawalRequest.sol";
+import { MockConsolidationStrategy } from "test/mocks/MockConsolidationStrategy.sol";
 
 /// @title Setup
 /// @notice Abstract contract responsible for test environment initialization and contract deployment.
@@ -41,7 +54,7 @@ abstract contract Setup is Base {
     /// --- ENVIRONMENT
     //////////////////////////////////////////////////////
     function _setUpRealisticEnvironnement() private {
-        vm.warp(1_750_000_000);
+        vm.warp(1_800_000_000);
         vm.roll(23_000_000);
     }
 
@@ -66,13 +79,33 @@ abstract contract Setup is Base {
 
         // Deploy WETH
         weth = new MockERC20("Wrapped Ether", "WETH", 18);
+        ssv = new MockERC20("SSV Network Token", "SSV", 18);
 
-        // Deploy external contracts ...
+        // Deploy mocks
+        mockSSVNetwork = new MockSSVNetwork();
+        mockBeaconRoots = new MockBeaconRoots();
+        mockBeaconOracle = new MockBeaconOracle();
+        mockBeaconProofs = new MockBeaconProofs();
+        mockDepositContract = new MockDepositContract();
+        mockWithdrawalRequest = new MockWithdrawalRequest();
+        mockConsolidationStrategy = new MockConsolidationStrategy();
+
+        // Mock as addresses
+        vault = makeAddr("Vault");
 
         vm.stopPrank();
 
         // Label all freshly deployed external contracts
         vm.label(address(weth), "WETH");
+        vm.label(address(ssv), "SSV");
+        vm.label(address(mockSSVNetwork), "Mock SSVNetwork");
+        vm.label(address(mockBeaconRoots), "Mock Beacon Roots");
+        vm.label(address(mockBeaconOracle), "Mock Beacon Oracle");
+        vm.label(address(mockBeaconProofs), "Mock Beacon Proofs");
+        vm.label(address(mockDepositContract), "Mock Deposit Contract");
+        vm.label(address(mockWithdrawalRequest), "Mock Withdrawal Request");
+        vm.label(address(mockConsolidationStrategy), "Mock Consolidation Strategy");
+        vm.label(vault, "Vault");
     }
 
     //////////////////////////////////////////////////////
@@ -81,7 +114,32 @@ abstract contract Setup is Base {
     function _deployContracts() private {
         vm.startPrank(deployer);
 
-        // Deploy contracts ...
+        // Deploy the Compounding Staking SSV Strategy proxy
+        proxy = new CompoundingStakingSSVStrategyProxy();
+
+        // Deploy the Compounding Staking SSV Strategy implementation
+        strategy = new CompoundingStakingSSVStrategy({
+            _baseConfig: InitializableAbstractStrategy.BaseStrategyConfig(address(0), vault),
+            _wethAddress: address(weth),
+            _ssvToken: address(ssv),
+            _ssvNetwork: address(mockSSVNetwork),
+            _beaconChainDepositContract: address(mockDepositContract),
+            _beaconOracle: address(mockBeaconOracle),
+            _beaconProofs: address(mockBeaconProofs)
+        });
+
+        // Initialize the proxy with the implementation address
+        address[] memory rewardsTokenAddresses = new address[](1);
+        rewardsTokenAddresses[0] = address(weth);
+        bytes memory data = abi.encodeWithSelector(
+            CompoundingStakingSSVStrategy.initialize.selector, rewardsTokenAddresses, new address[](0), new address[](0)
+        );
+        proxy.initialize({ _logic: address(strategy), _initGovernor: governor, _data: data });
+
+        vm.label(address(proxy), "Compounding Staking SSV Strategy Proxy");
+        vm.label(address(strategy), "Compounding Staking SSV Strategy Implementation");
+
+        strategy = CompoundingStakingSSVStrategy(payable(address(proxy)));
 
         vm.stopPrank();
     }
@@ -89,5 +147,12 @@ abstract contract Setup is Base {
     //////////////////////////////////////////////////////
     /// --- INITIALIZATION
     //////////////////////////////////////////////////////
-    function _initalize() private { }
+    function _initalize() private {
+        vm.startPrank(governor);
+
+        strategy.setRegistrator(governor);
+        strategy.addSourceStrategy(address(mockConsolidationStrategy));
+
+        vm.stopPrank();
+    }
 }
