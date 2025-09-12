655924f81b6683e33b2147e47ae09855e14c1d5b
Clément <clemmoller@gmail.com>
2025-09-12 15:52:16 +0200

    :sparkles: Add DepositContract for handling deposits in BeaconChain

---
+++ src/DepositContract.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.29;
+
+import { BeaconChain } from "./BeaconChain.sol";
+
+contract DepositContract {
+    ////////////////////////////////////////////////////
+    /// --- STORAGE
+    ////////////////////////////////////////////////////
+    BeaconChain public beaconChain;
+
+    ////////////////////////////////////////////////////
+    /// --- CONSTRUCTOR
+    ////////////////////////////////////////////////////
+    constructor(
+        address _beaconChain
+    ) {
+        beaconChain = BeaconChain(payable(_beaconChain));
+    }
+
+    ////////////////////////////////////////////////////
+    /// --- MUTATIVE FUNCTIONS
+    ////////////////////////////////////////////////////
+    receive() external payable { }
+
+    function deposit(
+        bytes memory pubkey,
+        bytes memory withdrawal_credentials,
+        bytes memory signature,
+        bytes32 deposit_data_root
+    ) external payable {
+        beaconChain.deposit{ value: msg.value }(pubkey, withdrawal_credentials, signature, deposit_data_root);
+    }
+}
