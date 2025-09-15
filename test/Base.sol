d0c71d673475124acd995f5b20fbc194b375da56
Clément <clemmoller@gmail.com>
2025-09-15 12:17:44 +0200

    :recycle: Split setup and variable storage.

--- test/Base.sol
+++ test/Base.sol
@@ -5,16 +5,22 @@ pragma solidity 0.8.29;
 import { Test } from "@forge-std/Test.sol";
 
 // Contract to test
+import { CompoundingStakingSSVStrategy } from "@origin-dollar/strategies/NativeStaking/CompoundingStakingSSVStrategy.sol";
+import { CompoundingStakingSSVStrategyProxy } from "@origin-dollar/proxies/Proxies.sol";
+
+// Mocks
+import { WETH } from "@solmate/tokens/WETH.sol";
 import { SSVNetwork } from "../src/SSVNetwork.sol";
 import { BeaconChain } from "../src/BeaconChain.sol";
 import { DepositContract } from "../src/DepositContract.sol";
 import { RewardDistributor } from "../src/RewardDistributor.sol";
 import { PartialWithdrawContract } from "../src/PartialWithdrawContract.sol";
 
-contract BaseTest is Test {
+contract Base is Test {
     ////////////////////////////////////////////////////
     /// --- CONSTANTS & IMMUTABLES
     ////////////////////////////////////////////////////
+    uint64 public constant GENESIS_TIMESTAMP = 1_606_824_023;
     address public constant DEPOSIT_CONTRACT_ADDRESS = 0x00000000219ab540356cBB839Cbe05303d7705Fa;
     address public constant WITHDRAWAL_REQUEST_ADDRESS = 0x00000961Ef480Eb55e80D19ad83579A64c007002;
 
@@ -23,6 +29,10 @@ contract BaseTest is Test {
     ////////////////////////////////////////////////////
     // Users
     address public alice = makeAddr("alice");
+    address public bobby = makeAddr("bobby");
+    address public deployer = makeAddr("deployer");
+    address public governor = makeAddr("governor");
+    address public operator = makeAddr("operator");
 
     // Validator pubkeys for testing, should be exactly 2 bytes long!
     bytes public validator1 = hex"0001";
@@ -31,38 +41,23 @@ contract BaseTest is Test {
     bytes public validator4 = hex"0004";
     bytes public validator5 = hex"0005";
 
+    // Mock
+    address public ssv = makeAddr("ssv");
+    address public oethVault = makeAddr("oethVault");
+    address public beaconProofs = makeAddr("beaconProofs"); // Todo
+
+    ////////////////////////////////////////////////////
+    /// --- CONTRACTS & MOCKS
+    ////////////////////////////////////////////////////
     // Contracts
+    CompoundingStakingSSVStrategy public strategy;
+    CompoundingStakingSSVStrategyProxy public strategyProxy;
+
+    // Mocks
+    WETH public weth;
     SSVNetwork public ssvNetwork;
     BeaconChain public beaconChain;
     DepositContract public depositContract;
     RewardDistributor public rewardDistributor;
     PartialWithdrawContract public partialWithdrawContract;
-
-    ////////////////////////////////////////////////////
-    /// --- SETUP
-    ////////////////////////////////////////////////////
-    function setUp() public virtual {
-        // First deploy BeaconChain contract
-        beaconChain = new BeaconChain();
-
-        // Deploy DepositContract and PartialWithdrawContract to their respective addresses on mainnet
-        deployCodeTo("DepositContract.sol", abi.encode(address(beaconChain)), DEPOSIT_CONTRACT_ADDRESS);
-        deployCodeTo("PartialWithdrawContract.sol", abi.encode(address(beaconChain)), WITHDRAWAL_REQUEST_ADDRESS);
-        depositContract = DepositContract(payable(DEPOSIT_CONTRACT_ADDRESS));
-        partialWithdrawContract = PartialWithdrawContract(payable(WITHDRAWAL_REQUEST_ADDRESS));
-
-        // Then deploy SSVNetwork contract
-        ssvNetwork = new SSVNetwork(address(beaconChain));
-
-        // Fetch RewardDistributor contract from BeaconChain and fund it heavily
-        rewardDistributor = beaconChain.REWARD_DISTRIBUTOR();
-        deal(address(beaconChain.REWARD_DISTRIBUTOR()), 1_000_000 ether);
-
-        deal(address(alice), 1_000_000 ether);
-        beaconChain.registerSsvValidator(validator1);
-        beaconChain.registerSsvValidator(validator2);
-        beaconChain.registerSsvValidator(validator3);
-        beaconChain.registerSsvValidator(validator4);
-        beaconChain.registerSsvValidator(validator5);
-    }
 }
