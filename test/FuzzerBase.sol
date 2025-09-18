c6acdbf68a3dead293f9f91c53eb539a7fbc738f
Clément <clemmoller@gmail.com>
2025-09-18 22:08:16 +0200

    :sparkles: Introduce FuzzerBase that stores Handler storage.

---
+++ test/FuzzerBase.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.29;
+
+/// @title FuzzerBase
+/// @notice Abstract base contract for fuzzing tests that hold all variable and helpers.
+/// @dev    This contract is inherited by concrete fuzzing contracts to share common setup and utilities.
+abstract contract FuzzerBase {
+    ////////////////////////////////////////////////////
+    /// --- CONSTANTS & IMMUTABLES
+    ////////////////////////////////////////////////////
+    uint256 public constant MAX_DEPOSITS = 12;
+
+    ////////////////////////////////////////////////////
+    /// --- STORAGE
+    ////////////////////////////////////////////////////
+    bytes[] public registeredSsvValidators;
+
+    ////////////////////////////////////////////////////
+    /// --- HELPERS
+    ////////////////////////////////////////////////////
+    function min(uint256 a, uint256 b) public pure returns (uint256) {
+        return a < b ? a : b;
+    }
+}
