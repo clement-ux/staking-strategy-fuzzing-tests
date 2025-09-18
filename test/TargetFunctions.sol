607cc31c497d00338431bef74de9a7c422fcafbc
Clément <clemmoller@gmail.com>
2025-09-18 17:08:36 +0200

    :sparkles: Update deposit handler to prevent zero deposits and add SSV validator registration functionality.

--- test/TargetFunctions.sol
+++ test/TargetFunctions.sol
@@ -6,6 +6,8 @@ import { Properties } from "test/Properties.sol";
 
 // Helpers
 import { console } from "@forge-std/console.sol";
+import { LibBytes } from "@solady/utils/LibBytes.sol";
+import { LibString } from "@solady/utils/LibString.sol";
 import { SafeCastLib } from "@solady/utils/SafeCastLib.sol";
 
 /// @title TargetFunctions
@@ -38,6 +40,8 @@ abstract contract TargetFunctions is Properties {
     // [ ] snapBalances
     // [ ] verifyBalances
     //
+    using LibBytes for bytes;
+    using LibString for string;
     using SafeCastLib for uint256;
 
     // ╔══════════════════════════════════════════════════════════════════════════════╗
@@ -51,7 +55,7 @@ abstract contract TargetFunctions is Properties {
         uint80 amount
     ) public {
         // Prevent deposit 0.
-        amount = _bound(amount, 1, amount).toUint80();
+        amount = _bound(amount, 1, type(uint80).max).toUint80();
 
         // Mint WETH directly to the strategy to simulate the vault having sent it.
         weth.mint(address(strategy), amount);
@@ -61,6 +65,42 @@ abstract contract TargetFunctions is Properties {
         strategy.deposit(address(weth), amount);
 
         // Log the deposit.
-        console.log("Deposit(): \t\t%18e", amount);
+        console.log("Deposit():   \t\t\t\t%18e", amount);
+    }
+
+    /// @notice Register a SSV validator.
+    /// @param index Index of the validator to register, limited to uint8 because strategy can only process 48 validators at
+    /// a time. uint8 = 255, so this is more than enough for 48 validators.
+    function handler_registerSsvValidator(
+        uint8 index
+    ) public {
+        // Bound the index of the validator to register.
+        index = _bound(index, 1, MAX_VALIDATORS).toUint8();
+
+        bytes memory pubkey;
+        // Ensure the validator is not already registered, if it is, try the next one, until we find one that is not
+        // registered.
+        for (uint256 i; i < MAX_VALIDATORS; i++) {
+            uint256 currentIndex = ((index - 1 + i) % MAX_VALIDATORS) + 1;
+            bytes memory _pubkey = indexToPubkey[currentIndex.toUint40()];
+            if (!beaconChain.ssvRegisteredValidators(_pubkey)) {
+                pubkey = _pubkey;
+                break;
+            }
+        }
+        // If all validators are already registered, skip the registration.
+        if (pubkey.eq(bytes(""))) {
+            // All validators are already registered.
+            console.log("RegisterSsvValidator(): \t\t all validators are already registered");
+            vm.assume(false);
+            return;
+        }
+
+        // Main call: registerSsvValidator
+        vm.prank(operator);
+        strategy.registerSsvValidator(pubkey, new uint64[](0), bytes(""), 0, emptyCluster);
+
+        // Log the registration.
+        console.log("RegisterSsvValidator(): \t\t", vm.toString(pubkey).slice(0, 5));
     }
 }
