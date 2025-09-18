4a65d5772f85f8abdf186e517e47113071503da4
Clément <clemmoller@gmail.com>
2025-09-18 12:06:17 +0200

    :art: Ignore some warnings

--- test/beacon_chain/Deposit.sol
+++ test/beacon_chain/Deposit.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.29;
 
 import { console } from "@forge-std/console.sol";
 import { Setup } from "../Setup.sol";
-import { BeaconChain } from "../../src/BeaconChain.sol";
 import { CompoundingValidatorManager } from "@origin-dollar/strategies/NativeStaking/CompoundingStakingSSVStrategy.sol";
 import { CompoundingStakingStrategyView } from "@origin-dollar/strategies/NativeStaking/CompoundingStakingView.sol";
 
